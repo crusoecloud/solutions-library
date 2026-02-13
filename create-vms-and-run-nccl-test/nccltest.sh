ffcdbfeb92dbfc15de22fd58b4b408d3df118bda
Abe Sharp <abe24seven@gmail.com>
2026-02-13 10:56:26 -0800

    Add UCX_NET_DEVICES env for B200 use case

--- create-vms-and-run-nccl-test/nccltest.sh
+++ create-vms-and-run-nccl-test/nccltest.sh
@@ -12,4 +12,5 @@ source /opt/hpcx/hpcx-init.sh
 hpcx_load
 /opt/hpcx/ompi/bin/mpirun --hostfile /home/ubuntu/hostfile -np $(cat /home/ubuntu/hostfile|wc -l) \
 	-x LD_LIBRARY_PATH=/opt/hpcx/ucx/lib:$LD_LIBRARY_PATH \
+#	-x UCX_NET_DEVICES=mlx5_5:1,mlx5_6:1,mlx5_7:1,mlx5_8:1,mlx5_9:1,mlx5_10:1,mlx5_11:1,mlx5_12:1 \ #uncomment only if using B200
 	/opt/nccl-tests/build/all_reduce_perf -b 1G -e 32G -f 2 -g $(nvidia-smi -L|wc -l)
