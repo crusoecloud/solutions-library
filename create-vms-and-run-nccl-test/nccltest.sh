c37e636a34866dd9a0714a166a7f9433e1316151
Abe Sharp <abe24seven@gmail.com>
2026-05-08 09:40:47 -0700

    Update NCCL topology file options in nccltest.sh
    Uncomment NCCL_TOPO_FILE options for different GPU types.

--- create-vms-and-run-nccl-test/nccltest.sh
+++ create-vms-and-run-nccl-test/nccltest.sh
@@ -15,6 +15,13 @@ export LD_LIBRARY_PATH=/opt/hpcx/ucx/lib:$LD_LIBRARY_PATH
 # export UCX_NET_DEVICES=mlx5_5:1,mlx5_6:1,mlx5_7:1,mlx5_8:1,mlx5_9:1,mlx5_10:1,mlx5_11:1,mlx5_12:1 # uncomment for B200 or adapt based on ibstat -v
 # export NCCL_IB_HCA=mlx5_5:1,mlx5_6:1,mlx5_7:1,mlx5_8:1,mlx5_9:1,mlx5_10:1,mlx5_11:1,mlx5_12:1 # uncomment for B200 or adapt based on ibstat -v
 # export NCCL_TOPO_FILE=/etc/crusoe/nccl_topo/b200-180gb-sxm-ib-cloud-hypervisor.xml # uncomment and changed per gpu type, ls /etc/crusoe/nccl_topo to see choices
+# export NCCL_TOPO_FILE=/etc/crusoe/nccl_topo/b300-288gb-sxm-ib-cloud-hypervisor.xml
+# export NCCL_TOPO_FILE=/etc/crusoe/nccl_topo/h200-141gb-sxm-ib-cloud-hypervisor.xml
+# export NCCL_TOPO_FILE=/etc/crusoe/nccl_topo/h100-80gb-sxm-ib-cloud-hypervisor.xml
+
+export NCCL_IB_MERGE_VFS=0
+export NCCL_SOCKET_NTHREADS=4
+export NCCL_NSOCKS_PERTHREAD=8
 
 NUM_NODES=$(cat /home/ubuntu/hostfile | wc -l)
 GPU_PER_NODE=$(nvidia-smi -L | wc -l)
